d0dcee2ad0baa63a1856076d22f8bf22b36d7d74
nando-cezar <nandocezar22@gmail.com>
2024-02-25 21:29:54 -0300

    auth google API

--- front_end/lib/pages/auth_page.dart
+++ front_end/lib/pages/auth_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:pandora_front/pages/login_page.dart';
 
 import 'home_page.dart';
+import 'login_or_register_page.dart';
 
 class AuthPage extends StatelessWidget{
   const AuthPage({super.key});
@@ -16,7 +17,7 @@ class AuthPage extends StatelessWidget{
           if(snapshot.hasData){
             return HomePage();
           }else{
-            return LoginPage();
+            return const LoginOrRegisterPage();
           }
         },
       )
