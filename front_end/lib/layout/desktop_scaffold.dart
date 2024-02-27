ea8dc2094674692098a54bb2de82e8b3c756dd3e
nando-cezar <nandocezar22@gmail.com>
2024-02-27 14:13:29 -0300

    responsive layout feature

--- front_end/lib/layout/desktop_scaffold.dart
+++ front_end/lib/layout/desktop_scaffold.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../components/my_box.dart';
+import '../components/my_tile.dart';
 import '../constants.dart';
 
 class DesktopScaffold extends StatefulWidget {
@@ -18,6 +21,48 @@ class _DesktopScaffoldState extends State<DesktopScaffold> {
       body: Row(
         children: [
           myDrawer,
+          Expanded(
+            flex: 2,
+            child: Column(
+              children: [
+                AspectRatio(
+                  aspectRatio: 4,
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: GridView.builder(
+                      itemCount: 4,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4,
+                      ),
+                      itemBuilder: (context, index) {
+                        return MyBox();
+                      },
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 5,
+                    itemBuilder: (context, index) {
+                      return MyTile();
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.pink,
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
--- front_end/lib/layout/tablet_scaffold.dart
+++ front_end/lib/layout/tablet_scaffold.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../components/my_box.dart';
+import '../components/my_tile.dart';
 import '../constants.dart';
 
 class TableScaffold extends StatefulWidget {
@@ -16,6 +18,33 @@ class _TableScaffoldState extends State<TableScaffold> {
       appBar: myAppBar,
       backgroundColor: myDefaultBackground,
       drawer: myDrawer,
+      body: Column(
+        children: [
+          AspectRatio(
+            aspectRatio: 4,
+            child: SizedBox(
+              width: double.infinity,
+              child: GridView.builder(
+                itemCount: 4,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                ),
+                itemBuilder: (context, index) {
+                  return MyBox();
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return MyTile();
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
